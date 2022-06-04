6018e6167c0543e0f836b900dd57e9c3f8fe57f5
bortkyle18 <bortkyle18@gmail.com>
2022-06-04 17:54:08 -0400

    webpage template and file create

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArgs.length; i+= 1) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-
-    console.log('===============')
+const [name, github] = profileDataArgs;
 
 
-    //Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
